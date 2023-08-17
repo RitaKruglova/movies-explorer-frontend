db622771d9feebbede9453819d4b301f104626ff
Rita Kruglova <kruglova404@mail.ru>
2023-08-17 17:27:10 +0300

    feat: create Logo component

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <Routes>
+      
+    </Routes>
+  );
+}
+export default App;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import logo from '../../images/logo.svg';
+import { useNavigate } from 'react-router-dom';
+
+function Logo() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/');
+  }
+  return (
+    <img
+      src={logo}
+      alt='Логотип'
+      className='logo'
+      onClick={handleClick}
+    />
+  );
+}
