5bde1b89fa1b8072b75059678c091edaac3d1bcb
Rita Kruglova <kruglova404@mail.ru>
2023-08-17 20:09:57 +0300

    feat: create BurgerButton component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,31 @@
-
+import AccountButton from "../AccountButton/AccountButton";
+import Logo from "../Logo/Logo";
+import Navigation from "../Navigation/Navigation";
+import BurgerButton from "../BurgerButton/BurgerButton";
+import { useState } from "react";
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  function toggleMenuVisibility() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  return (
+    <header className="header">
+      <Logo />
+      {
+        window.innerWidth > 768
+        ?
+        <>
+          <Navigation />
+          <AccountButton />
+        </>
+        :
+        <BurgerButton onClick={toggleMenuVisibility} isOpen={isMenuOpen} />
+      }
+    </header>
+  );
 }
 
 export default Header;
