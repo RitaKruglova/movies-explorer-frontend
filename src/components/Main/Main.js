98d0296813f26841a8a0a6230a7940ec3a75500e
Rita Kruglova <kruglova@404@mail.ru>
2023-08-18 17:31:53 +0300

    feat: create AboutProject component

---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import Header from "../Header/Header";
+import Promo from "../Promo/Promo";
+
+function Main() {
+  <main className="main">
+    <Header />
+    <Promo />
+  </main>
+}
+
+export default Main;
