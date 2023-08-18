3566c43df91867f256bdfcb433894db2e2a1f5bf
Rita Kruglova <kruglova@404@mail.ru>
2023-08-18 17:37:32 +0300

    feat: create Techs component

---
+++ src/components/Techs/Techs.js
@@ -0,0 +1,19 @@
+function Techs() {
+  return (
+    <section className="techs">
+      <h3 className="techs__title">7 технологий</h3>
+      <p className="techs__subtitle">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+      <ul className="techs__list">
+        <li className="techs__list-cell">HTML</li>
+        <li className="techs__list-cell">CSS</li>
+        <li className="techs__list-cell">JS</li>
+        <li className="techs__list-cell">React</li>
+        <li className="techs__list-cell">Git</li>
+        <li className="techs__list-cell">Express.js</li>
+        <li className="techs__list-cell">mongoDB</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Techs;
