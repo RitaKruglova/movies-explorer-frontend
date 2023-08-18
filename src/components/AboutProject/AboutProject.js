c00510a28ee7ab174512c80ef0d7e1d0afc8d5fc
Rita Kruglova <kruglova404@mail.ru>
2023-08-18 17:31:53 +0300

    feat: create AboutProject component

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,24 @@
+function AboutProject() {
+  return (
+    <section className="about-project">
+      <div className="about-project__line">
+        <h2 className="about-project__title">О проекте</h2>
+      </div>
+      <ul className="about-project__description">
+        <li
+          className="about-project__description-cell">Дипломный проект включал 5 этапов</li>
+        <li className="about-project__description-cell">На выполнение диплома ушло 5 недель</li>
+        <li className="about-project__description-cell about-project__description-cell_place_bottom">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</li>
+        <li className="about-project__description-cell about-project__description-cell_place_bottom">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</li>
+      </ul>
+      <ul className="about-project__table">
+        <li className="about-project__table-cell about-project__table-cell_color_green">1 неделя</li>
+        <li className="about-project__table-cell about-project__table-cell_color_grey">4 недели</li>
+        <li className="about-project__table-cell">Back-end</li>
+        <li className="about-project__table-cell">Front-end</li>
+      </ul>
+    </section>
+  )
+}
+
+export default AboutProject;
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -6,7 +6,7 @@ function Promo() {
     <section className="promo">
       <div className="promo__info">
         <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
-        <h2 className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</h2>
+        <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
         <NavTab />
       </div>
       <img
