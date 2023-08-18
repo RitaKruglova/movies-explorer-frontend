0b55af50c8169a9b3f87fdf7c9c94a03338f23a7
Rita Kruglova <kruglova404@mail.ru>
2023-08-18 17:37:32 +0300

    feat: create Techs component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,14 @@
+import AboutProject from "../AboutProject/AboutProject";
 import Header from "../Header/Header";
 import Promo from "../Promo/Promo";
+import Techs from "../Techs/Techs";
 
 function Main() {
   <main className="main">
     <Header />
     <Promo />
+    <AboutProject />
+    <Techs />
   </main>
 }
 
