4cec5659c436048c28285f3734ed80d3c363209b
Rita Kruglova <kruglova@404@mail.ru>
2023-08-19 19:12:03 +0300

    feat: create Footer component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,27 @@ import AccountButton from "../AccountButton/AccountButton";
 import Logo from "../Logo/Logo";
 import Navigation from "../Navigation/Navigation";
 import BurgerButton from "../BurgerButton/BurgerButton";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DropdownMenu from "../DropdownMenu/DropdownMenu";
+import { Link } from "react-router-dom";
+import HeaderAuth from "../HeaderAuth/HeaderAuth";
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   function toggleMenuVisibility() {
     setIsMenuOpen(!isMenuOpen);
@@ -15,19 +31,24 @@ function Header() {
   return (
     <header className="header">
       <Logo />
-      {
-        window.innerWidth > 768
-        ?
-        <>
-          <Navigation needMainLink={false}/>
-          <AccountButton />
-        </>
-        :
-        <>
-          <BurgerButton onClick={toggleMenuVisibility} isOpen={isMenuOpen} />
-          {isMenuOpen && <DropdownMenu />}
-        </>
-      }
+        {windowWidth > 768 && isAuthorized && (
+          <>
+            <Navigation />
+            <AccountButton />
+          </>
+        )}
+        {windowWidth > 768 && !isAuthorized && (
+          <HeaderAuth />
+        )}
+        {windowWidth <= 768 && isAuthorized && (
+          <>
+            <BurgerButton onClick={toggleMenuVisibility} isOpen={isMenuOpen} />
+            {isMenuOpen && <DropdownMenu />}
+          </>
+        )}
+        {windowWidth <= 768 && !isAuthorized && (
+          <HeaderAuth />
+        )}
     </header>
   );
 }
