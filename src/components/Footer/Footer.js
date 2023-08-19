330ba009bb07e83a603aacde830e09658342212d
Rita Kruglova <kruglova404@mail.ru>
2023-08-19 19:12:03 +0300

    feat: create Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+  <footer className="footer">
+    <h5 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h5>
+    <div className="footer__info">
+      <p className="footer__date">© 2023</p>
+      <div className="footer__references-container">
+        <p className="footer__platform">Яндекс.Практикум</p>
+        <p className="footer__platform">Github</p>
+      </div>
+    </div>
+  </footer>
+}
