72f95d51ab57f17ba498db4dadc0b1a8208b68a5
Rita Kruglova <kruglova@404@mail.ru>
2023-08-22 20:25:38 +0300

    feat: add styles for about-project for all resolutions

---
+++ src/components/GlobeImg/GlobeImg.js
@@ -0,0 +1,13 @@
+import webImage from '../../images/web.svg';
+
+function GlobeImg() {
+  return (
+    <img
+      src={webImage}
+      alt="Глобус, сложенный из слов 'web'"
+      className="globe-img"
+    />
+  )
+}
+
+export default GlobeImg;
