f38ba5dd399873bda1a79b8003b81bff6be0ef6b
Rita Kruglova <kruglova404@mail.ru>
2023-08-28 14:23:29 +0300

    feat: add styles for movies-card-list for all resolutions

---
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -0,0 +1,21 @@
+import MoviesCard from "../MoviesCard/MoviesCard";
+
+function MoviesCardList() {
+  return (
+    <section className="movies-card-list">
+      <MoviesCard />
+      <MoviesCard />
+      <MoviesCard />
+      <MoviesCard />
+      <MoviesCard />
+      <MoviesCard />
+      <MoviesCard />
+      <MoviesCard />
+      <MoviesCard />
+      <MoviesCard />
+      <MoviesCard />
+    </section>
+  )
+}
+
+export default MoviesCardList;
