75e78ce248d8126350b076a916626c3962ef56e6
Rita Kruglova <kruglova@404@mail.ru>
2023-08-28 17:52:08 +0300

    feat: create SavedMovies component and add styles for one

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/movies" element={<Movies />} />
+        <Route path="/saved-movies" element={<SavedMovies />} />
       </Routes>
     </div>
   );
--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,24 +1,46 @@
 import { useState } from 'react';
 import movieCover from '../../images/cover.jpg';
 
-function MoviesCard() {
+function MoviesCard({isSavedMoviesPlace = false}) {
   const [isLiked, setIsLiked] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   function handleLikeClick() {
     setIsLiked(!isLiked);
   }
 
+  function handleMouseEnter() {
+    setIsHovered(true);
+  }
+
+  function handleMouseLeave() {
+    setIsHovered(false);
+  }
+
   return(
-    <article className="movies-card">
+    <article
+      className="movies-card"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <div className="movies-card__container">
         <img className="movies-card__image" src={movieCover} alt="Постер фильма" />
         <div className="movies-card__info">
           <h2 className="movies-card__title">Твоё имя</h2>
-          <button
-            className={`movies-card__button ${isLiked ? " movies-card__button_active" : ""}`}
-            type="button"
-            onClick={handleLikeClick}
-          />
+          {isSavedMoviesPlace
+            ?
+            <button
+              className={`movies-card__button movies-card__button_place_saved-movies${isHovered ? " movies-card__button_visible" : ""}`}
+              type="button"
+              onClick={handleLikeClick}
+            />
+            :
+            <button
+              className={`movies-card__button ${isLiked ? " movies-card__button_active" : ""}`}
+              type="button"
+              onClick={handleLikeClick}
+            />
+          }
         </div>
       </div>
       <p className="movies-card__duration">1ч50м</p>
