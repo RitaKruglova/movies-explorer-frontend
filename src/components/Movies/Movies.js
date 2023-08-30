6d2bb04915a24fec84b5ba6ed1c0833a85c72b9c
Rita Kruglova <kruglova404@mail.ru>
2023-08-30 12:47:21 +0300

    feat: create Preloader component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Preloader from '../Preloader/Preloader';
 import SearchForm from '../SearchForm/SearchForm';
 import ShowMoreButton from '../ShowMoreButton/ShowMoreButton';
 
 function Movies({toggleMenuVisibility, isDropdownMenuOpen}) {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <div className="movies">
+      {isLoading && <Preloader />}
       <div className="movies__container">
         <Header isAccountButtonWhite={true} toggleMenuVisibility={toggleMenuVisibility} isDropdownMenuOpen={isDropdownMenuOpen}/>
         <SearchForm isDropdownMenuOpen={isDropdownMenuOpen} />
