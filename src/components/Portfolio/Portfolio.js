b9b47a0a6b4737bafa92d8bede245b4aa7666823
Rita Kruglova <kruglova404@mail.ru>
2023-09-01 21:54:07 +0300

    fix: take out headed and footer from main and fix links

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,19 +5,19 @@ function Portfolio() {
         <h4 className="portfolio__title">Портфолио</h4>
         <ul className="portfolio__links">
           <li className="portfolio__list-item">
-            <a className="portfolio__link" href="https://github.com/RitaKruglova/how-to-learn">
+            <a className="portfolio__link" target="blank" href="https://github.com/RitaKruglova/how-to-learn">
               Статичный сайт
               <span className="portfolio__arrow">↗</span>
             </a>
           </li>
           <li className="portfolio__list-item">
-            <a className="portfolio__link" href="https://ritakruglova.github.io/russian-travel/">
+            <a className="portfolio__link" target="blank" href="https://ritakruglova.github.io/russian-travel/">
               Адаптивный сайт
               <span className="portfolio__arrow">↗</span>
             </a>
           </li>
           <li className="portfolio__list-item">
-            <a className="portfolio__link" href="https://mesto.rita-kruglova.nomoredomains.xyz/">
+            <a className="portfolio__link" target="blank" href="https://mesto.rita-kruglova.nomoredomains.xyz/">
               Одностраничное приложение
               <span className="portfolio__arrow">↗</span>
             </a>
