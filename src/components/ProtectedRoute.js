9b3dd620ea26e6990d03df212efadbe605045282
Rita Kruglova <kruglova@404@mail.ru>
2023-09-04 14:46:12 +0300

    feat: create ProtectedRoute component

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+import { LoggedInContext } from "../contexts/LoggedInContext";
+import { useContext } from "react";
+
+function ProtectedRoute({element: Component, ...props}) {
+  const loggedIn = useContext(LoggedInContext);
+
+  return (
+    loggedIn ? <Component {...props} /> : <Navigate to="/signin" replace />
+  )
+}
+
+export default ProtectedRoute;
