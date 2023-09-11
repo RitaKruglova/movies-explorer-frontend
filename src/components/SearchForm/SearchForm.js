d16d3d20a347fade41f5861fefaba6873da2e92d
Rita Kruglova <kruglova@404@mail.ru>
2023-09-11 15:10:17 +0300

    feat: add functionality of search

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 
-function SearchForm({isDropdownMenuOpen}) {
+function SearchForm({isDropdownMenuOpen, searchInputValue, handleChange, handleSubmit, errorText, handleCheckboxChange, isShort}) {
+  
+
   return (
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit} noValidate>
         <div className="search-form__container">
           <input
             className="search-form__input"
             type="text"
             placeholder="Фильм"
             required
+            onChange={handleChange}
+            value={searchInputValue}
           />
           <button
             className="search-form__button"
@@ -18,8 +23,12 @@ function SearchForm({isDropdownMenuOpen}) {
             Найти
           </button>
         </div>
-        <span className="search-form__error"></span>
-        <FilterCheckbox isDropdownMenuOpen={isDropdownMenuOpen} />
+        <span className="search-form__error">{errorText}</span>
+        <FilterCheckbox
+          isDropdownMenuOpen={isDropdownMenuOpen}
+          handleCheckboxChange={handleCheckboxChange}
+          isShort={isShort}
+        />
       </form>
   );
 }
